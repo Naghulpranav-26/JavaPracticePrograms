fceff2b617d83d6730f9a183df20faf4a7a1d8d1
Naghulpranav-26 <naghulnarayani91@gmail.com>
2026-01-17 19:58:09 +0530

    Added Shadowing2.java

---
+++ Shadowing2.java
@@ -0,0 +1,8 @@
+public class Shadowing2 {
+    static  int x=20;
+    public static void main(String[] args) {
+//        System.out.println(x);
+            x=45;
+        System.out.println(x);
+    }
+}
